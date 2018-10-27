d0192ae6db6809f0934a69d01cca697a03f901fa
wazt <waztfm@gmail.com>
2018-10-27 12:44:05 +0300

    Initial commit

---
+++ SEO/SEO/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SEO.Startup))]
+namespace SEO
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}

11f346f4b01c3bc9d260c0df4396ed9e37c8deea
Mihai Gurau <mihaigurau97@gmail.com>
2018-10-27 23:37:39 +0300

    Added GetAvailableRooms

--- SEO/SEO/App_Code/Business/RoomData.cs
+++ SEO/SEO/App_Code/Business/RoomData.cs
@@ -43,6 +43,39 @@ public class RoomData
         return rooms;
     }
 
+    public static List<Room> GetAvailableRooms(DateTime StartTime, DateTime EndTime)
+    {
+        List<Room> rooms = new List<Room>();
+
+        string connectionString = Database.GetConnectionString();
+        using (SqlConnection conn = new SqlConnection(connectionString))
+        {
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("spGetAvailableRooms", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            
+            cmd.Parameters.Add(new SqlParameter("@pmId", StartTime));
+            cmd.Parameters.Add(new SqlParameter("@pmBuildingId", EndTime));
+
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Room room = new Room();
+                    room.Id = Convert.ToInt32(reader["Id"]);
+                    room.BuildingId = Convert.ToInt32(reader["BuildingId"]);
+                    room.Name = reader["Name"].ToString();
+                    room.Floor = Convert.ToInt32(reader["Floor"]);
+                    rooms.Add(room);
+                }
+            }
+            conn.Close();
+        }
+
+        return rooms;
+    }
+
     public static bool UpdateRoom(Room room)
     {
         bool result;

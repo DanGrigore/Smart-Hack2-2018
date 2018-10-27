c66cab51931e37ec8e994e241808018c5b41d378
Mihai Gurau <mihaigurau97@gmail.com>
2018-10-27 23:10:37 +0300

    Added the rest of the Bussines Objects

--- SEO/SEO/App_Code/Business/SeatData.cs
+++ SEO/SEO/App_Code/Business/SeatData.cs
@@ -5,45 +5,48 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
-public class RoomData
+public class SeatData
 {
-    public static List<Room> GetRooms(int? id = null, int? BuildingId = null, int? Floor = null)
+    public static List<Seat> GetSeats(int? id = null, int? RoomId = null, int? Ox = null, int? Oy = null)
     {
-        List<Room> rooms = new List<Room>();
+        List<Seat> seats = new List<Seat>();
 
         string connectionString = Database.GetConnectionString();
         using (SqlConnection conn = new SqlConnection(connectionString))
         {
             conn.Open();
 
-            SqlCommand cmd = new SqlCommand("spGetRooms", conn);
+            SqlCommand cmd = new SqlCommand("'spGetSeats", conn);
             cmd.CommandType = CommandType.StoredProcedure;
 
             if (id.HasValue)
                 cmd.Parameters.Add(new SqlParameter("@pmId", id.Value));
-            if (BuildingId.HasValue)
-                cmd.Parameters.Add(new SqlParameter("@pmBuildingId", BuildingId.Value));
-            if (Floor.HasValue)
-                cmd.Parameters.Add(new SqlParameter("@pmFloor", Floor.Value));
+            if (RoomId.HasValue)
+                cmd.Parameters.Add(new SqlParameter("@pmRoomId", RoomId.Value));
+            if (Ox.HasValue)
+                cmd.Parameters.Add(new SqlParameter("@pmOx", Ox.Value));
+            if (Oy.HasValue)
+                cmd.Parameters.Add(new SqlParameter("@pmOy", Oy.Value));
 
             using (SqlDataReader reader = cmd.ExecuteReader())
             {
                 while (reader.Read())
                 {
-                    Room room = new Room();
-                    room.Id = Convert.ToInt32(reader["Id"]);
-                    room.BuildingId = Convert.ToInt32(reader["BuildingId"]);
-                    room.Name = reader["Name"].ToString();
-                    room.Floor = Convert.ToInt32(reader["Floor"]);
-                    rooms.Add(room);
+                    Seat seat = new Seat();
+                    seat.Id = Convert.ToInt32(reader["Id"]);
+                    seat.RoomId = Convert.ToInt32(reader["RoomId"]);
+                    seat.Ox = Convert.ToInt32(reader["Ox"]);
+                    seat.Oy = Convert.ToInt32(reader["Oy"]);
+                    seats.Add(seat);
                 }
             }
             conn.Close();
         }
-        return rooms;
+
+        return seats;
     }
 
-    public static bool UpdateRoom(Room room)
+    public static bool UpdateSeat(Seat seat)
     {
         bool result;
         string connectionString = Database.GetConnectionString();
@@ -51,13 +54,13 @@ public class RoomData
         {
             conn.Open();
 
-            SqlCommand cmd = new SqlCommand("spUpdateRoom", conn);
+            SqlCommand cmd = new SqlCommand("spUpdateSeat", conn);
             cmd.CommandType = CommandType.StoredProcedure;
 
-            cmd.Parameters.Add(new SqlParameter("@pmId", room.Id));
-            cmd.Parameters.Add(new SqlParameter("@pmBuildingId", room.BuildingId));
-            cmd.Parameters.Add(new SqlParameter("@pmFloor", room.Floor));
-            cmd.Parameters.Add(new SqlParameter("@pmName", room.Name));
+            cmd.Parameters.Add(new SqlParameter("@pmId", seat.Id));
+            cmd.Parameters.Add(new SqlParameter("@pmRoomId", seat.RoomId));
+            cmd.Parameters.Add(new SqlParameter("@pmOx", seat.Ox));
+            cmd.Parameters.Add(new SqlParameter("@pmOy", seat.Oy));
 
             int noRows = cmd.ExecuteNonQuery();
             result = (noRows == 0) ? false : true;
@@ -66,7 +69,7 @@ public class RoomData
         return result;
     }
 
-    public static int InsertRoom(Room room)
+    public static int InsertSeat(Seat seat)
     {
         int newId;
         string connectionString = Database.GetConnectionString();
@@ -76,10 +79,10 @@ public class RoomData
 
             SqlCommand cmd = new SqlCommand("spInsertRoom", conn);
             cmd.CommandType = CommandType.StoredProcedure;
-            
-            cmd.Parameters.Add(new SqlParameter("@pmBuildingId", room.BuildingId));
-            cmd.Parameters.Add(new SqlParameter("@pmFloor", room.Floor));
-            cmd.Parameters.Add(new SqlParameter("@pmName", room.Name));
+
+            cmd.Parameters.Add(new SqlParameter("@pmRoomId", seat.RoomId));
+            cmd.Parameters.Add(new SqlParameter("@pmOx", seat.Ox));
+            cmd.Parameters.Add(new SqlParameter("@pmOy", seat.Oy));
 
             newId = Convert.ToInt32(cmd.ExecuteScalar());
             conn.Close();
